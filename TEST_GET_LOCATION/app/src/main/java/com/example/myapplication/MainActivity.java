a4122cd1645134c56e6e73c3a5853c06a7b31021
Gradi0us <hieunguyen20032302@gmail.com>
2022-11-01 10:31:52 +0700

    hehe

--- TEST_GET_LOCATION/app/src/main/java/com/example/myapplication/MainActivity.java
+++ TEST_GET_LOCATION/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -84,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
     //acccccccccccccccccccccccccccccc
+    //abcgchgccgg
     public void Map(View view) {
         Intent intent = new Intent(MainActivity.this,Map.class);
         intent.putExtra("url","http://www.google.com/maps/search/?api=1&query=37.4219983%2C-122.084");
